48dbe1ebaa6b91dceee9098b2dbae42d07f81f97
Gustavo12386 <gucalderaro@hotmail.com>
2025-10-09 17:05:52 -0300

    Add lambdahandler

---
+++ src/main/java/com/service/config/handler/LambdaHandler.java
@@ -0,0 +1,32 @@
+package com.service.config.handler;
+
+import com.service.ServiceSystemApplication;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler = null;
+
+    static {
+        try {
+			handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+			        .defaultProxy()
+			        .springBootApplication(ServiceSystemApplication.class)
+			        .build();
+		} catch (ContainerInitializationException e) {			
+			e.printStackTrace();
+		}
+    }
+
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
+        return handler.proxy(awsProxyRequest, context);
+    }
+}
